b97a9b52e81ee54b1588be88f51d278fcc7a7b54
webexpert821 <jollydev0821@gmail.com>
2023-03-02 05:40:40 +0900

    test mode is created

--- src/config/test.ts
+++ src/config/test.ts
@@ -2,6 +2,6 @@ import contracts from '../contracts/contracts.json'
 
 export const isTest = false;
 
-export const apiName = isTest ? 'testwebhooks' :'webhooks';
+export const apiName = isTest ? 'testwebhooks' :'webhooks'
 
 export const contractAddy = isTest ? contracts.KingFlokiNFTs.testnet_address : contracts.KingFlokiNFTs.mainnet_address;
